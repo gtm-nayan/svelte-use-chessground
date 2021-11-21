710baff714ae05d0a7ba40118210de020582d8b2
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-11-21 16:30:20 +0545

    add dests demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,17 +2,46 @@
 	import { Chessground, cgStylesHelper } from '$lib/index';
 	import '$lib/cgstyles/chessground.css';
 	import type { Config } from '$lib/chessground/config.js';
+	import { Chess } from 'chess.js';
+	import { randomMove, validMovesAsDests } from './_utils';
+	import type { Api } from '$lib/chessground/api';
+	import type { Key, MoveMetadata } from '$lib/chessground/types';
 
+	let chess = Chess();
+
+	let cgApi: Api;
 	let config: Config = {
 		orientation: 'white',
+		movable: {
+			color: 'white',
+			free: false,
+			events: {
+				after: handleMove
+			}
+		},
 		events: {
-			init: console.log
+			init: (api) => {
+				cgApi = api;
+				cgApi.state.movable.dests = validMovesAsDests(chess);
+			}
 		}
 	};
+
+	function handleMove(from: Key, to: Key, metadata: MoveMetadata) {
+		chess.move(`${from}${to}`, { sloppy: true });
+
+		let move = chess.move(randomMove(chess), { verbose: true });
+		cgApi.move(move.from, move.to);
+		cgApi.state.turnColor = 'white';
+		cgApi.state.movable.dests = validMovesAsDests(chess);
+	}
 </script>
 
 <div
 	use:Chessground={config}
 	class="blue"
-	use:cgStylesHelper={{ piecesFolderUrl: '/images/pieces/merida', boardUrl: '/images/board/blue.svg' }}
+	use:cgStylesHelper={{
+		piecesFolderUrl: '/images/pieces/merida',
+		boardUrl: '/images/board/blue.svg'
+	}}
 />
